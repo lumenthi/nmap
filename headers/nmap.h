3bb3b2ec64c043599ec6e944ab6877f2effa7b84
lumenthi <lumenthi@student.42.fr>
2022-09-22 13:28:51 -0400

    :construction: Trying to send raw TCP packets

--- headers/nmap.h
+++ headers/nmap.h
@@ -16,6 +16,7 @@
 #include <sys/time.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/udp.h>
+#include <netinet/tcp.h>
 
 /* struct sockaddr {
 	ushort	sa_family;
@@ -41,6 +42,63 @@ typedef struct	s_data {
 	char				*address;
 
 }						t_data;
+
+/* struct iphdr
+{
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+	unsigned int	ihl:4;
+	unsigned int	version:4;
+#elif __BYTE_ORDER == __BIG_ENDIAN
+	unsigned int	version:4;
+	unsigned int	ihl:4;
+#else
+# error        "Please fix <bits/endian.h>"
+#endif
+	u_int8_t	tos;
+	u_int16_t	tot_len;
+	u_int16_t	id;
+	u_int16_t	frag_off;
+	u_int8_t	ttl;
+	u_int8_t	protocol;
+	u_int16_t	check;
+	u_int32_t	saddr;
+	u_int32_t	daddr;
+}; */
+
+/* struct tcphdr {
+	__be16	source;
+	__be16	dest;
+	__be32	seq;
+	__be32	ack_seq;
+#if defined(__LITTLE_ENDIAN_BITFIELD)
+	__u16	res1:4,
+		doff:4,
+		fin:1, FLAGS
+		syn:1,
+		rst:1,
+		psh:1,
+		ack:1,
+		urg:1,
+		ece:1,
+		cwr:1;
+#elif defined(__BIG_ENDIAN_BITFIELD)
+	__u16	doff:4,
+		res1:4,
+		cwr:1,
+		ece:1,
+		urg:1,
+		ack:1,
+		psh:1,
+		rst:1,
+		syn:1,
+		fin:1;
+#else
+#error	"Adjust your <asm/byteorder.h> defines"
+#endif
+	__be16	window;
+	__sum16	check;
+	__be16	urg_ptr;
+}; */
 
 /* nmap.c */
 int		ft_nmap(char *destination, uint8_t args,
--- sources/nmap.c
+++ sources/nmap.c
@@ -24,15 +24,90 @@ static int resolve(char *host, t_data *g_data)
 	return 0;
 }
 
+static unsigned short csum(unsigned short *buf, int len)
+{
+	unsigned long sum;
+
+	for(sum=0; len>0; len--)
+		sum += *buf++;
+	sum = (sum >> 16) + (sum &0xffff);
+	sum += (sum >> 16);
+	return (unsigned short)(~sum);
+}
+
+static void send_syn(int sockfd, t_data *g_data)
+{
+	unsigned int len = 0;
+
+	char packet[sizeof(struct iphdr)+sizeof(struct tcphdr)+len];
+	struct iphdr *ip = (struct iphdr *)packet;
+	struct tcphdr *tcp = (struct tcphdr *)(packet+sizeof(struct iphdr));
+	(void)g_data;
+
+	ft_memset(packet, 0, sizeof(packet));
+
+	/* Version */
+	ip->version = 4;
+	/* Internet Header Length (how many 32-bit words are present in the header) */
+	ip->ihl = sizeof(struct iphdr) / sizeof(uint32_t);
+	/* Type of service */
+	ip->tos = 0;
+	/* Total length */
+	ip->tot_len = sizeof(packet);
+	/* TODO: Identification (check notes.txt) */
+	ip->id = htons(56508);
+	/* TODO: Set don't fragment flag ! IP Flags + Fragment offset */
+	ip->frag_off = 0;
+	/* TTL */
+	ip->ttl = 64;
+	/* Protocol (TCP) */
+	ip->protocol = IPPROTO_TCP;
+	/* Checksum */
+	ip->check = 0; /* TODO: Calculate it after the TCP header */
+	/* Source ip */
+	ip->saddr = *(uint32_t *)&g_data->servaddr.sin_addr;
+	/* Dest ip */
+	ip->daddr = *(uint32_t *)&g_data->servaddr.sin_addr;
+
+	/* Source port */
+	tcp->source = htons(57686);
+	/* Destination port */
+	tcp->dest = *(uint16_t *)&g_data->servaddr.sin_addr;
+	/* Seq num */
+	tcp->seq = htonl(1);
+	/* Ack num */
+	tcp->ack_seq = 0;
+	/* Sizeof header */
+	tcp->doff = 10;
+	/* SYN */
+	tcp->syn = 1;
+	/* WTF is this */
+	tcp->window = htons(64240);
+	/* Checksum */
+	tcp->check = 0;
+	/* Indicates the urgent data, only if URG flag set */
+	tcp->urg_ptr = 0;
+
+	/* Checksums */
+	ip->check = csum((unsigned short *)packet, sizeof(packet));
+
+	printf("TCP: %ld\n", sizeof(struct tcphdr));
+	printf("len: %d\n", ip->tot_len);
+	printf("proto: %d\n", ip->protocol);
+
+	sendto(sockfd, packet, ip->tot_len, 0, g_data->host_addr, sizeof(struct sockaddr_in));
+	write(sockfd, packet, sizeof(packet));
+}
+
 int syn_scan(char *destination, uint16_t port)
 {
 	(void)port;
 	t_data g_data = {0};
 	int sockfd;
-	int ttl = -1;
+	int one = 1;
 
 	/* Socket creation */
-	if ((sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+	if ((sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_TCP)) < 0) {
 		fprintf(stderr, "%s: Failed to create TCP socket\n", destination);
 		return 1;
 	}
@@ -40,23 +115,33 @@ int syn_scan(char *destination, uint16_t port)
 	/* Resolving host */
 	if (resolve(destination, &g_data)) {
 		fprintf(stderr, "%s: Name or service not known\n", destination);
+		close(sockfd);
 		return 1;
 	} /* g_data.host_info is allocated ! Must free it now */
 
 	/* Set options */
-	if ((setsockopt(sockfd, SOL_IP, IP_TTL, &ttl, sizeof(ttl))) != 0) {
+	if ((setsockopt(sockfd, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one))) != 0) {
 		fprintf(stderr, "%s: Failed to set TTL option\n", destination);
+		freeaddrinfo(g_data.host_info);
+		close(sockfd);
 		return 1;
 	}
-
-	printf("%s: %s\n", destination, g_data.ipv4);
 
 	g_data.servaddr.sin_family = AF_INET;
 	g_data.servaddr.sin_port = htons(port);
 	g_data.host_addr = (struct sockaddr *)&g_data.servaddr;
 
-	int ret = connect(sockfd, g_data.host_addr, sizeof(g_data.servaddr));
-	printf("Connect: %d\n", ret);
+	/* int ret = connect(sockfd, g_data.host_addr, sizeof(g_data.servaddr));
+	if (ret != 0) {
+		fprintf(stderr, "%s: Failed to connect to host\n", destination);
+		freeaddrinfo(g_data.host_info);
+		close(sockfd);
+		return 1;
+	}*/ 
+
+	send_syn(sockfd, &g_data);
+
+	printf("%s: %s\n", destination, g_data.ipv4);
 
 	freeaddrinfo(g_data.host_info);
 	close(sockfd);
